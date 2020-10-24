35e685696fed324982ff0d925b4667cdb71e7c61
Harley Hanna <harleyhannawrit@gmail.com>
2020-10-24 19:40:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,8 +19,8 @@ class App < Sinatra::Base
   end
   
   get '/multiply/:number1/:number2' do
-    @num1 = params[:number1]
-    @num2 = params[:number2]
+    @num1 = params[:number1].to_s
+    @num2 = params[:number2].to_s
     @num1 * @num2
   end
 
